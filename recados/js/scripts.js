8dbc36a00d5bb8f75f6256abfb769f8f42eec0bc
Carla Silva <carla.silva@movestock.com>
2023-12-11 15:13:28 -0300

    added put and delete

--- recados/js/scripts.js
+++ recados/js/scripts.js
@@ -16,9 +16,28 @@ async function fetchMessages() {
       messageCard.innerHTML = `
         <h2 class="card-title">${message.title}</h2>
         <p class="card-description">${message.description}</p>
+        <div class="card-icons">
+          <i class="fas fa-solid fa-trash" data-id=${message.id}></i>
+          <i class="fas fa-regular fa-edit" data-id=${message.id}></i>
+        </div>
       `
 
       messagesContainer.appendChild(messageCard)
+
+      const deleteIcon = messageCard.querySelector('.fa-trash')
+
+      deleteIcon.addEventListener('click', () => {
+        const messageId = deleteIcon.getAttribute('data-id')
+
+        deleteMessage(messageId)
+      })
+
+      const editIcon = messageCard.querySelector('.fa-edit')
+      editIcon.addEventListener('click', () => {
+        const messageId = editIcon.getAttribute('data-id')
+
+        navigateToEditPage(messageId)
+      })
     });
 
     if (messages.length === 0) {
@@ -33,43 +52,7 @@ async function fetchMessages() {
 
 fetchMessages()
 
-
-
-// createNewMessage()
-
-async function updateMessage() {
-  const editMessage = {
-    title: 'Novo Título',
-    description: 'Uma descrição legal'
-  }
-
-  try {
-    const idMessage = 156
-    const response = await api.put(`/notes/${idMessage}`, editMessage)
-
-    if(response.status === 200) {
-      alert('Recado atualizado com sucesso!')
-    }
-  }catch (error) {
-    console.log('Erro ao atualizar recado.')
-  }
+function navigateToEditPage(messageId) {
+  location.href = `editar-recado.html?id=${messageId}`
 }
 
-// updateMessage()
-
-async function deleteMessage() {
-  const idMessage = 109
-
-  try {
-    const response = await api.delete(`/notes/${idMessage}`)
-
-    if (response.status === 200) {
-      alert('Recado exclído com sucesso')
-    }
-
-  } catch (error) {
-    console.log('Erro ao excluir recado', error)
-  }
-}
-// deleteMessage()
-// fetchMessages()
